07c9d8f45306d11e656fb802a6bdda64c70e3c25
JoanAndoni <mcr_joan@hotmail.com>
2018-11-08 09:36:19 -0600

    Latest changes

--- BasicPointers/basicpointers.c
+++ BasicPointers/basicpointers.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-				int a = 5;
+	int a = 5;
 	int* ptr = NULL;
 
 	ptr = &a;
---
+++ ImagesManipulationThreads/test_ppm.c
@@ -0,0 +1,153 @@
+/*
+    Modify by:
+    Joan Andoni González Rioz
+    A00569929
+
+    Sample program to test libraries.
+    Uses images in PPM format:
+     http://netpbm.sourceforge.net/doc/ppm.html
+
+    Gilberto Echeverria
+    gilecheverria@yahoo.com
+    31/01/2018
+*/
+
+#include <unistd.h>
+#include <getopt.h>
+#include "string_functions.h"
+#include "ppms.h"
+
+void usage(char * program);
+void parseOptions(int argc, char * argv[]);
+void imageOperations(char * in_filename, char * out_filename, char * matrix_filename, int doMatrix, int doNegative, int rotationAngle, int newScale);
+
+int main(int argc, char * argv [])
+{
+    printf("\n=== PROGRAM TO TEST PPM READING AND WRITING ===\n");
+
+    parseOptions(argc, argv);
+
+    return 0;
+}
+
+void usage(char * program)
+{
+    printf("Usage:\n");
+    printf("\t%s -i {input_file} -o {output_file} [-f {matrix_file}] [-n] [-r {degrees}] [-s {percentage}]\n", program);
+    printf("Available operations:\n");
+    printf("\t-f : Apply the matrix filter into the image\n");
+    printf("\t-n : Compute the negative of an image\n");
+    printf("\t-r : Rotate the image the specified degrees (90, 180, 270)\n");
+    printf("\t-s : Scale the image up or down a percentage (50, 200)\n");
+    exit(EXIT_FAILURE);
+}
+
+void parseOptions(int argc, char * argv[])
+{
+    int opt;
+    char * in_filename;
+    char * out_filename;
+    char * matrix_filename;
+    int input_ok = 0;
+    int output_ok = 0;
+    int doMatrix = 0;
+    int doNegative = 0;
+    int rotationAngle = 0;
+    int newScale = 0;
+
+    while ( (opt = getopt(argc, argv, "i:o:f:nr:s:")) != -1 )
+    {
+        switch (opt)
+        {
+            case 'i':
+                in_filename = optarg;
+                input_ok = 1;
+                break;
+            case 'o':
+                out_filename = optarg;
+                output_ok = 1;
+                break;
+            case 'f':
+                matrix_filename = optarg;
+                doMatrix = 1;
+                break;
+            case 'n':
+                doNegative = 1;
+                break;
+            case 's':
+                newScale = atoi(optarg);
+                break;
+            case 'r':
+                rotationAngle = atoi(optarg);
+                break;
+            default:
+                printf("Invalid option '%c'\n", opt);
+                usage(argv[0]);
+                break;
+        }
+
+    }
+
+    if (input_ok && output_ok)
+    {
+        // Call the function to do image operations
+        imageOperations(in_filename, out_filename, matrix_filename, doMatrix, doNegative, rotationAngle, newScale);
+    }
+    else
+    {
+        usage(argv[0]);
+    }
+}
+
+void imageOperations(char * in_filename, char * out_filename, char * matrix_filename, int doMatrix, int doNegative, int rotationAngle, int newScale)
+{
+    // Create the image variables and initialize to be empty
+    ppm_t input_image = {"", 0, 0, 0, NULL};
+    matrix_t filter = {0, 0, NULL};
+
+    // Load the image into the program
+    readImageFile(in_filename, &input_image);
+    printf("Read file '%s' successfully\n", in_filename);
+
+#ifdef DEBUG
+    // The file just read
+    printf("THESE ARE THE PIXELS\n");
+    printPixels(&input_image);
+#endif /* DEBUG */
+
+    if (doMatrix)
+    {
+        // Transform the image into a negative
+        readMatrixFile(matrix_filename, &filter);
+
+        // Transform the image with the filter send
+        applyMatrixFilter(&input_image, &filter);
+
+        free (filter.data);
+    }
+
+    if (doNegative)
+    {
+        // Transform the image into a negative
+        getNegativeImage(&input_image);
+    }
+
+    if (rotationAngle)
+    {
+        // Rotate the image
+        rotateImage(&input_image, rotationAngle);
+    }
+
+    if (newScale)
+    {
+        // Scale down the image
+        scaleImage(&input_image, newScale);
+    }
+
+    // Save the new image
+    writeImageFile(out_filename, &input_image);
+    printf("Wrote file '%s' successfully\n", out_filename);
+
+    // Liberate the memory used for the structure
+    freeMemory( &input_image );
+}
